8685d8c3ba05f9eea61f2e082a64adf1a2937707
David-Vaclavik <geny1794@gmail.com>
2025-10-10 20:36:26 +0200

    feat: scroll to top on route change
    Add automatic scroll reset when navigating between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./styles/App.css";
 import "./styles/Variables.css";
 import { useState, useEffect } from "react";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
@@ -10,10 +10,15 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchedProduct, setSearchedProduct] = useState(null);
   const [cart, setCart] = useState([]);
+  const location = useLocation();
 
-  //TODO: increase limit once project is complete
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   // Limit to 20 products for faster load during development, 0 is unlimited
-  const LIMIT = 20;
+  const LIMIT = 0;
 
   useEffect(() => {
     const categories = ["laptops", "smartphones", "mobile-accessories", "tablets"];
@@ -48,25 +53,8 @@ function App() {
   useEffect(() => {
     // fetch(`https://dummyjson.com/products`)
     // fetch(`https://dummyjson.com/products?limit=${LIMIT}&sortBy=title&order=asc`)
-
-    fetch(
       `https://dummyjson.com/products/category/smartphones?limit=${LIMIT}&sortBy=title&order=asc`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => setData(data))
-      .catch((error) => {
-        console.error("Failed to fetch products:", error);
-      });
-  }, []);
   */
-
-  // console.log(data);
-  // console.log(data.products[0]);
 
   return (
     <>
